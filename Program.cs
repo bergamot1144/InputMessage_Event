4064a9a90893260df706ba12a713c6d69a564722
berga <bergamot.ads1@gmail.com>
2024-08-13 19:30:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+string message = "this text is for testing событие in my program";
+MessageHandler handler = new MessageHandler();
+
+handler.OnEventOccured += () =>
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Событие произошло!");
+    Console.ResetColor();
+};
+
+
+handler.CheckMessage(message);
+
+
+
+public delegate void EventOccured();
+
+public class MessageHandler
+{
+    public event EventOccured OnEventOccured;
+
+    public void CheckMessage(string message)
+    {
+        string[] wordsInMessage = message.Split(' ');
+
+
+        foreach (string word in wordsInMessage)
+        {
+
+            if(word == "событие")
+            {
+                if (OnEventOccured != null)
+                {
+                    OnEventOccured.Invoke();
+                }
+            }
+            else
+            {
+                Console.WriteLine($"{word} - Не найдено");
+            }
+        }
+    }
+
+}
